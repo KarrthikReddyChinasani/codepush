bd4bebcff2ebd27fd219385d11d6faa63d35d8ac
Karthik Reddy <karthikreddy@nineleaps.com>
2020-01-30 02:42:25 +0530

    second commit

--- App.js
+++ App.js
@@ -76,7 +76,7 @@ const styles = StyleSheet.create({
     right: 0,
   },
   body: {
-    backgroundColor: Colors.white,
+    backgroundColor: "#000000",
   },
   sectionContainer: {
     marginTop: 32,
--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -23,10 +23,10 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
-          return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),	       
-          new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG)
-      );	      
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // packages.add(new MyReactNativePackage());
+          return packages;
         }
 
         @Override
